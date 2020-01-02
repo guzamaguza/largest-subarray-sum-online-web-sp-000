c01edc2ac14d4882d61e6e739b95babdfe219e3d
Mark A Guza <guzamaguza@gmail.com>
2020-01-02 16:55:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function largestSubarraySum(array){
 let positiveSum = 0;
-//let biggestNeg = 0;
+let biggestNeg = -1000;
 
   for(let i=0; i<array.length; i++){
     if(array[i] > 0){
@@ -13,7 +13,7 @@ let positiveSum = 0;
   //let array[j-1] = 0;
   for(let j=0; j<array.lenth; j++){
     if(array[j] < 0){
-        let biggestNeg = -1000;
+        //let biggestNeg = -1000;
         if(array[j] > biggestNeg){
           biggestNeg = array[j];
         }
